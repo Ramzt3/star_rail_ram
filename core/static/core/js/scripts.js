ba5af8f37e53b909e57380342326acd87dae69b5
siliphong-mac <it@BOOKNEAs-Mac-mini.local>
2024-01-24 17:26:27 +0700

    fixed card and added footer

--- core/static/core/js/scripts.js
+++ core/static/core/js/scripts.js
@@ -18,7 +18,19 @@ function myFunction() {
 	}
 }
 
+
 function multiCheckbox() {
 	const form = document.getElementById("filter-form");
 	console.log(form);
+}
+
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function mbNav() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
 }
